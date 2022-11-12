6bd330a1b5bf2883a741f1fcbe7481ca2eea3021
saeedalseiari <36856434+saeedalseiari@users.noreply.github.com>
2022-11-12 06:59:54 +0400

    Update settings.py

--- rad_tools/settings.py
+++ rad_tools/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+import dj_database_url
 import os
 
 
@@ -121,10 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/")]
-
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
